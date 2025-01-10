4335d5a1c75d5720bcb3ad5f3d91f208660e8051
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2025-01-10 17:19:40 +0000

    improve handling of coords

--- src/ibl_style/utils.py
+++ src/ibl_style/utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 MM_TO_INCH = 1 / 25.4
 
@@ -53,18 +54,20 @@ def get_coords(extent, ratios=[1], space=15, pad=7.5, span=(0, 1)):
     >>> get_coords(extent=100, ratios=[1, 1, 2], space=[5, 10], pad=7.5)
     [[0.075, 0.26875], [0.31875, 0.5125], [0.6124, 1.0]]
     """
+    span = np.array(span) * 100
     full_span = span[1] - span[0]
 
     if isinstance(space, list):
-        space = [s / extent for s in space]
+        #space = [s / extent for s in space]
+        space = [np.round((s / extent) * 100) for s in space]
     else:
-        space = [space / extent] * (len(ratios) - 1)
+        space = [np.round((space / extent) * 100)] * (len(ratios) - 1)
 
-    pad = pad / extent
+    pad = np.round((pad / extent * 100))
 
     white_space = sum(space)
-    available_space = (1 - white_space - pad) * full_span
-    panel_span = available_space / sum(ratios)
+    available_space = (full_span - white_space - pad)
+    panel_span = np.round(available_space / sum(ratios))
 
     # Get the coordinates of the first panel
     coords = [[pad + span[0], pad + span[0] + panel_span * ratios[0]]]
@@ -72,7 +75,7 @@ def get_coords(extent, ratios=[1], space=15, pad=7.5, span=(0, 1)):
     for i, r in enumerate(ratios[1:]):
         coords.append([coords[i][-1] + space[i], coords[i][-1] + space[i] + panel_span * r])
 
-    return coords
+    return np.array(coords) / 100 + 1 / 10000
 
 
 def add_label(text, fig, xspan, yspan, padx=2.5, pady=2.5, fontsize=8):
@@ -130,7 +133,7 @@ def add_label(text, fig, xspan, yspan, padx=2.5, pady=2.5, fontsize=8):
     width, height = fig.get_size_inches() / MM_TO_INCH
 
     def _get_label_pos(dimension, coord, pad):
-        return coord - pad / dimension
+        return np.max([0, coord - pad / dimension])
 
     label = {
         'label_text': text,
